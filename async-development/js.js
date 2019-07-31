d7d3e2f1729f5016db7f2bf4a87f7e001377e542
Irina <zaletk.irina@gmail.com>
2019-07-31 22:52:29 +0300

    weather api base init

---
+++ async-development/js.js
@@ -0,0 +1,25 @@
+var button = document.getElementById('button');
+button.addEventListener('click',get);
+var input = document.getElementById('weather_Search');
+var output = document.getElementById('output');
+var img = document.createElement('img');
+const par = document.createElement('p');
+par.classList.add('description');
+
+function get(){
+    fetch('http://api.apixu.com/v1/current.json?key=c79c30965ca349f89b3181040193007&q=' + document.getElementById('weather_Search').value)
+    .then(response=>response.json())
+    .then((data) => {
+        par.innerHTML =`${data.location.country}, ${data.location.name}: ${data.current.temp_c} °C, ${data.current.temp_c}, ${data.current.condition.text}`;
+        img.src=`http:${data.current.condition.icon}`;
+        output.append(img);
+        output.append(par);
+    })
+    .catch((error) => {
+        //debugger;
+        //console.error(error)
+        output.innerHTML = 'Something went wrong or city you are trying to check is not valid';
+    })
+}
+
+
